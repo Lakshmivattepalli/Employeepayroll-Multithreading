22ada876f997bd4dc08039e06c73fcaca14e92a4
srikala <vlslaksh1899@gmail.com>
2022-04-26 14:38:14 +0530

    added employee payroll using multithreading file

--- EMPLOYEEPAYROLLTESTING/UnitTest1.cs
+++ EMPLOYEEPAYROLLTESTING/UnitTest1.cs
@@ -1,30 +1,30 @@
-﻿using System;
+using EmployeePayRollSystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 
-
-namespace EmployeePayRollSystem
+namespace EmployeePayrollTest
 {
-    class Program
+    [TestClass]
+    public class UnitTest1
     {
-        static void Main(string[] args)
+        [TestMethod]
+        public void Given10Employee_WhenAddedToList_ShouldMatchEmployeeEntries()
         {
             List<EmployeeDetails> employeeDetails = new List<EmployeeDetails>();
             employeeDetails.Add(new EmployeeDetails(EmployeeID: 1, EmployeeName: "Lakshmi1", PhoneNumber: "9014120136", Address: "VDP", Department: "PROD", Gender: 'F', BasicPay: 3423, Deduction: 544, TaxablePay: 5456, Tax: 4565, NetPay: 5656, City: "banglore", Country: "india"));
             employeeDetails.Add(new EmployeeDetails(EmployeeID: 1, EmployeeName: "Lakshmi2", PhoneNumber: "9014120136", Address: "VDP", Department: "PROD", Gender: 'F', BasicPay: 3423, Deduction: 544, TaxablePay: 5456, Tax: 4565, NetPay: 5656, City: "banglore", Country: "india"));
             employeeDetails.Add(new EmployeeDetails(EmployeeID: 1, EmployeeName: "Lakshmi3", PhoneNumber: "9014120136", Address: "VDP", Department: "PROD", Gender: 'F', BasicPay: 3423, Deduction: 544, TaxablePay: 5456, Tax: 4565, NetPay: 5656, City: "banglore", Country: "india"));
             employeeDetails.Add(new EmployeeDetails(EmployeeID: 1, EmployeeName: "Lakshmi4", PhoneNumber: "9014120136", Address: "VDP", Department: "PROD", Gender: 'F', BasicPay: 3423, Deduction: 544, TaxablePay: 5456, Tax: 4565, NetPay: 5656, City: "banglore", Country: "india"));
-            employeeDetails.Add(new EmployeeDetails(EmployeeID: 1, EmployeeName: "Lakshmi5", PhoneNumber: "9014120136", Address: "VDP", Department: "PROD", Gender: 'F', BasicPay: 3423, Deduction: 544, TaxablePay: 5456, Tax: 4565, NetPay: 5656, City: "banglore", Country: "india"));
+            employeeDetails.Add(new EmployeeDetails(EmployeeID: 1, EmployeeName: "Lakshmi5", PhoneNumber: "9014120136", Address: "VDP", Department: "PROD", Gender: 'F',BasicPay: 3423, Deduction: 544, TaxablePay: 5456, Tax: 4565, NetPay: 5656, City: "banglore", Country: "india"));
             employeeDetails.Add(new EmployeeDetails(EmployeeID: 1, EmployeeName: "Lakshmi6", PhoneNumber: "9014120136", Address: "VDP", Department: "PROD", Gender: 'F', BasicPay: 3423, Deduction: 544, TaxablePay: 5456, Tax: 4565, NetPay: 5656, City: "banglore", Country: "india"));
             employeeDetails.Add(new EmployeeDetails(EmployeeID: 1, EmployeeName: "Lakshmi7", PhoneNumber: "9014120136", Address: "VDP", Department: "PROD", Gender: 'F', BasicPay: 3423, Deduction: 544, TaxablePay: 5456, Tax: 4565, NetPay: 5656, City: "banglore", Country: "india"));
             employeeDetails.Add(new EmployeeDetails(EmployeeID: 1, EmployeeName: "Lakshmi8", PhoneNumber: "9014120136", Address: "VDP", Department: "PROD", Gender: 'F', BasicPay: 3423, Deduction: 544, TaxablePay: 5456, Tax: 4565, NetPay: 5656, City: "banglore", Country: "india"));
             employeeDetails.Add(new EmployeeDetails(EmployeeID: 1, EmployeeName: "Lakshmi9", PhoneNumber: "9014120136", Address: "VDP", Department: "PROD", Gender: 'F', BasicPay: 3423, Deduction: 544, TaxablePay: 5456, Tax: 4565, NetPay: 5656, City: "banglore", Country: "india"));
             employeeDetails.Add(new EmployeeDetails(EmployeeID: 1, EmployeeName: "Lakshmi10", PhoneNumber: "9014120136", Address: "VDP", Department: "PROD", Gender: 'F', BasicPay: 3423, Deduction: 544, TaxablePay: 5456, Tax: 4565, NetPay: 5656, City: "banglore", Country: "india"));
 
-
             EmployeePayRollOperation employeePayRollOperation = new EmployeePayRollOperation();
             employeePayRollOperation.addEmployeeToPayRoll(employeeDetails);
-            Console.WriteLine("======================================================");
-            employeePayRollOperation.addEmployeeToPayRollWithThread(employeeDetails);
         }
     }
 }
+
